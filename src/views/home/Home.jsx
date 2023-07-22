06dc3bdd49e595c92d1214fd944a0d9e328ed509
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2023-07-22 14:21:36 -0300

    add changes

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import HeaderMenu from '../../components/HeaderMenu/HeaderMenu';
 import Search from '../../components/Search/Search'
 import Productscard from '../../components/ProductsCard/Productscard';
-
+import "./index.scss";
 
 const Home = ({data}) => {
   return (
